d24d05ee81e2c317dd3f584659cf7a98460bb95a
jujubahalvex <juliaalves.dev2024@gmail.com>
2025-03-13 13:35:14 -0300

    Meu sexto projeto em javascript

---
+++ js/soma.js
@@ -0,0 +1,8 @@
+function somar(){
+    if(true){
+        let n1 = parseFloat(document.getElementById("num1").value);
+        let n2 = parseFloat(document.getElementById("num2").value);
+        let soma = n1 + n2;
+        document.getElementById("resultado").textContent = "Resultado: " + soma;
+    }
+}
