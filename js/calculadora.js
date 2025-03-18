e1b02bccb58179ce94d1f9481676f7129e711f40
jujubahalvex <juliaalves.dev2024@gmail.com>
2025-03-18 09:08:01 -0300

    Atividade

---
+++ js/calculadora.js
@@ -0,0 +1,17 @@
+function appendValue(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    try {
+        // Vai fazer a conta e voltar o resultado
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch {
+        // Em caso de erro, exibe uma mensagem de erro
+        document.getElementById('display').value = 'Erro';
+    }
+}
